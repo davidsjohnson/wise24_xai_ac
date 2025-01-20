99e80b0618ae9b55df1aae1d5c565cbbc2be44b8
David Johnson <djohnson@techfak.uni-bielefeld.de>
2025-01-20 12:13:19 +0100

    Added Base Audio Notebook and Resources

---
+++ evaluate.py
@@ -0,0 +1,35 @@
+import random
+
+import numpy as np
+import torch
+def evaluate_model(model, dataloader, criterion, device):
+    model.eval()  # Set model to evaluation mode
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    preds = []
+    probs = []
+    with torch.no_grad():  # Disable gradient calculation
+        for inputs, labels, idxs in dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            # Track loss and accuracy
+            running_loss += loss.item()
+            prop_dist = torch.nn.Softmax(dim=-1)(outputs)
+            prob, predicted = prop_dist.max(1)
+            
+            preds.extend(predicted.cpu().numpy())
+            probs.extend(prob.cpu().numpy())
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+
+    avg_loss = running_loss / len(dataloader)
+    accuracy = correct / total
+
+    print(f"Evaluation Loss: {avg_loss:.4f}, Evaluation Accuracy: {accuracy:.4f}")
+    return avg_loss, accuracy, preds, probs
